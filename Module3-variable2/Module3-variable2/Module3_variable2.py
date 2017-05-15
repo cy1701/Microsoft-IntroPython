40409cc8b7fa314e6870bbd8565c6ad3be57a573
cy1701 <cy1701@gmail.com>
2017-05-14 20:59:19 -0400

    module3

---
+++ Module3-variable2/Module3-variable2/Module3_variable2.py
@@ -0,0 +1,11 @@
+#variables
+# first test
+#name = input ('what is your name ? ')
+#print (name)
+
+# saying context is king
+#
+#
+FirstName = input ('Enter your first name? ')
+LastName = input ('Enter your last name? ')
+print ('Hello ' + FirstName + ' ' + LastName + ' how is your day')
